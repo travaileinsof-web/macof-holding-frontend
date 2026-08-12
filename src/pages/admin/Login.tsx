6d33a2c5e6f211eb3db91ece646713e531648ea2
Billy Doumbouya <192708154+Clinton62395@users.noreply.github.com>
2026-08-12 13:57:01 +0000

    first commit

--- src/pages/admin/Login.tsx
+++ src/pages/admin/Login.tsx
@@ -17,7 +17,7 @@ export default function Login() {
     setLoading(true);
 
     try {
-      const response = await api.post('/admin/login', {
+      const response = await api.post('/auth/login', {
         email,
         password
       });
